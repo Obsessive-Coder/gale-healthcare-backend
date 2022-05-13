eceb68f86795ae52972fc3d25fb38c7af0cf5544
Obsessive-Coder <jaredhuff85@gmail.com>
2022-05-13 05:09:38 -0500

    Question 2
    * Solve and return the first querry

--- index.php
+++ index.php
@@ -18,21 +18,3 @@ $router->get('/question2', function ($request) {
 $router->post('/data', function ($request) {
   return json_encode($request->getBody());
 });
-
-// DB Options.
-// $host = 'localhost';
-// $username = 'root';
-// $password = '';
-// $database = 'gale_healthcare_db';
-
-// // Connect to the server and database.
-// $connection = mysqli_connect($host, $username, $password, $database);
-
-// $recText = $_POST['text'];
-// $query = "INSERT INTO user(username) VALUES('$recText')";
-
-// if (mysqli_query($connection, $query)) {
-//   echo 'Added new user.';
-// } else {
-//   echo 'Error adding new user.';
-// }
--- util/solutions/Question_2.php
+++ util/solutions/Question_2.php
@@ -6,6 +6,23 @@ class Question_2 implements IQuestion
 {
   public static function solve()
   {
-    echo 'Answer 2';
+    // DB Options.
+    $host = 'localhost';
+    $username = 'root';
+    $password = '';
+    $database = 'gale_healthcare_db';
+
+    // Connect to the server and database.
+    $connection = mysqli_connect($host, $username, $password, $database);
+
+    $query1 = "SELECT u.userid, u.username, u.email, group_concat(ph.password SEPARATOR ',') passwords FROM user u JOIN passwordhistory ph ON ph.userid = u.userid GROUP BY ph.userid;";
+
+    $result1 = mysqli_query($connection, $query1);
+    if ($result1) {
+      $jsonResult1 = mysqli_fetch_all($result1, MYSQLI_ASSOC);
+      echo json_encode($jsonResult1);
+    } else {
+      echo 'Error querying the database';
+    }
   }
 }
