8302a98450fbbbc155bc2b9ee1d252fc0e939224
Obsessive-Coder <jaredhuff85@gmail.com>
2022-05-14 03:52:02 -0500

    refacatored routes

--- index.php
+++ index.php
@@ -5,15 +5,17 @@ include_once 'router/Router.php';
 include_once 'util/solutions/Question_1.php';
 include_once 'util/solutions/Question_2.php';
 
+// Used below to bind routes to their handlers.
+const ROUTES = array(
+  'question1' => new Question_1(),
+  'question2' => new Question_2(),
+);
+
 $router = new Router(new Request);
 
-$router->get('/question1', function ($request) {
-  return Question_1::solve();
-});
-
-$router->get('/question2', function ($request) {
-  return Question_2::solve();
-});
+foreach (ROUTES as $key => $RouteHandler) {
+  $router->get('/' . $key, fn ($request) => $RouteHandler::solve());
+}
 
 $router->post('/data', function ($request) {
   return json_encode($request->getBody());
