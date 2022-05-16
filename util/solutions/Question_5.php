564c3ca31f4de522e9f346902cf86c203e2330ec
Obsessive-Coder <jaredhuff85@gmail.com>
2022-05-15 19:02:15 -0500

    Added question 5

--- util/solutions/Question_5.php
+++ util/solutions/Question_5.php
@@ -6,6 +6,6 @@ class Question_5 implements IQuestion
 {
   public function solve()
   {
-    echo 'TODO: Solve Question 5';
+    echo 'If I get an error while trying to run code locally and the database won\'nt connect I first ensure the database server and related services are running. After I confirm the service is running I will double check my database credentials and configuration. If this still doesn\'t work I will dig deeper into the error and leverage Google. I have always been able to connect to a database using this approach.';
   }
 }
