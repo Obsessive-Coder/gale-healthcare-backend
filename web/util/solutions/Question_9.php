cabaae7e3572461a4fe747339c8de41d2430f468
Obsessive-Coder <jaredhuff85@gmail.com>
2022-05-16 02:56:13 -0500

    finish question 9

--- web/util/solutions/Question_9.php
+++ web/util/solutions/Question_9.php
@@ -6,6 +6,6 @@ class Question_9 implements IQuestion
 {
   public function solve()
   {
-    return 'TODO: Solve Question 9';
+    return 'This project... For this code challenge I built a full stack website that uses React for the client hosted on Github Pages, PHP on the backend hosted on Heroku, and a MYSQL database hosted on AWS RDS. I created a basic API with PHP which I have never used and shown the ability to use AWS as my last interviewers requested. Because of this is a "full" project I am using it as my submission for this question.';
   }
 }
