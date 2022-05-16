04685e991f392a336b628671d63265131c24e0bb
Obsessive-Coder <jaredhuff85@gmail.com>
2022-05-15 19:37:22 -0500

    Finsihed question 8

--- util/solutions/Question_8.php
+++ util/solutions/Question_8.php
@@ -6,12 +6,12 @@ class Question_8 implements IQuestion
 {
   public function solve()
   {
-    // A URL shortener is a tool that takes a long and complex URL and compresses it to a short and clear link that is easier to share.
+    echo 'I have never made a url shortener so I wouldn\'t be able to write the function without first following a tutorial. I am all about tutorials and followed several for the PHP portions of these challenges. However, I feel this would be too specific and I would use too much of the code from the tutorial.
 
-    // The process of URL shortening is pretty simple. URL shorteners generate a random string, map the main URL to the shortcode and provide a new URL. The two URLs are put in the database.
+    Considering this, I still did some research and feel that I understand enough to describe a large portion of the steps required to achieve this.
+    
 
-    // When you hit the shortened URL, the database checks the shortcode and redirects you to the main URL. The URL shortener gets a long URL and returns a short URL.
 
-    echo 'I began with researching url shorteners and getting a basic understanding of how they function. Then I created 1 table in the existing database for this project to store the data about the url. The database table has columns "url" and "short_url". After the short url is generated and stored, the original url can be used to retrieve the shortened url.';
+    I would create 1 table in a database for this project to store the url, short url, and a unique id. After the short url is generated and stored, the original url can be used to retrieve the shortened url. Generating the url doesn\'t seem too difficult and is a matter of converting the long url into readable a string by mapping the url chars to letters and numbers, but I must admit I didn\'t fully understand the code examples that I looked at. In the real world I would spend enough time to implement some of the code examples I saw and come up with a unique solution that solves the company\'s/client\'s needs.';
   }
 }
